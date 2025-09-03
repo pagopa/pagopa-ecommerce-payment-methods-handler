31afee924cbbcdc595303d33c51958790d2f3295
CianoDanilo <danilo.ciano@pagopa.it>
2025-09-03 10:40:07 +0200

    [CHK-4403] feat(code-review): add code review pipeline files (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkus:quarkus-jacoco")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-resteasy-client")
     implementation("io.quarkus:quarkus-resteasy-mutiny")
