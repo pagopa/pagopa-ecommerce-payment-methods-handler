0186783ad1809f70982dbfba442810675b124159
Forciniti Luigi <forciniti@spi.srl>
2026-01-20 11:05:25 +0100

    chore: correct this.group.toString() in this.group

--- src/main/kotlin/it/pagopa/ecommerce/payment/methods/mappers/PaymentMethodMappers.kt
+++ src/main/kotlin/it/pagopa/ecommerce/payment/methods/mappers/PaymentMethodMappers.kt
@@ -60,7 +60,7 @@ fun PaymentMethodResponseDto.toPaymentMethodResponse(): PaymentMethodResponse {
      * future re-enablement when AFM provides fee ranges. this.rangeAmount?.let {
      * afmResponseFeeRange -> FeeRange().max(afmResponseFeeRange.max).min(afmResponseFeeRange.min) }
      */
-    paymentHandlerPaymentMethod.paymentTypeCode = this.group.toString()
+    paymentHandlerPaymentMethod.paymentTypeCode = this.group
     paymentHandlerPaymentMethod.paymentMethodAsset = this.paymentMethodAsset
     paymentHandlerPaymentMethod.methodManagement =
         PaymentMethodResponse.MethodManagementEnum.valueOf(this.methodManagement.toString())
