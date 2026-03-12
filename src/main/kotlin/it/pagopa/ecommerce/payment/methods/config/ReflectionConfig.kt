852fcb250e092f74797e79e271824abd769e8c40
Forciniti Luigi <forciniti@spi.srl>
2026-03-12 17:04:47 +0100

    chore: add only PaymentOptionMultiDto in RegisterForReflection

--- src/main/kotlin/it/pagopa/ecommerce/payment/methods/config/ReflectionConfig.kt
+++ src/main/kotlin/it/pagopa/ecommerce/payment/methods/config/ReflectionConfig.kt
@@ -6,10 +6,7 @@ import io.quarkus.runtime.annotations.RegisterForReflection
     classNames =
         [
             "it.pagopa.ecommerce.payment.methods.v1.server.model.ProblemJson",
-            "it.pagopa.generated.ecommerce.client.model.PaymentNoticeItemDto",
-            "it.pagopa.generated.ecommerce.client.model.TransferListItemDto",
             "it.pagopa.generated.ecommerce.client.model.PaymentOptionMultiDto",
-            "it.pagopa.generated.ecommerce.client.model.PspSearchCriteriaDto",
         ]
 )
 class ReflectionConfig {}
