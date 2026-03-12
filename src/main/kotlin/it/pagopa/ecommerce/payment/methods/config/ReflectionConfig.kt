ba44221d06ce83535f0ab9d8070e817db6573c40
Forciniti Luigi <forciniti@spi.srl>
2026-03-12 15:46:50 +0100

    chore: add PaymentNoticeItemDto,TransferListItemDto,                   PaymentOptionMultiDto,PspSearchCriteriaDto in RegisterForReflection

--- src/main/kotlin/it/pagopa/ecommerce/payment/methods/config/ReflectionConfig.kt
+++ src/main/kotlin/it/pagopa/ecommerce/payment/methods/config/ReflectionConfig.kt
@@ -2,5 +2,19 @@ package it.pagopa.ecommerce.payment.methods.config
 
 import io.quarkus.runtime.annotations.RegisterForReflection
 import it.pagopa.ecommerce.payment.methods.v1.server.model.ProblemJson
+import it.pagopa.generated.ecommerce.client.model.PaymentNoticeItemDto
+import it.pagopa.generated.ecommerce.client.model.PaymentOptionMultiDto
+import it.pagopa.generated.ecommerce.client.model.PspSearchCriteriaDto
+import it.pagopa.generated.ecommerce.client.model.TransferListItemDto
 
-@RegisterForReflection(targets = [ProblemJson::class]) class ReflectionConfig {}
+@RegisterForReflection(
+    targets =
+        [
+            ProblemJson::class,
+            PaymentNoticeItemDto::class,
+            TransferListItemDto::class,
+            PaymentOptionMultiDto::class,
+            PspSearchCriteriaDto::class,
+        ]
+)
+class ReflectionConfig {}
