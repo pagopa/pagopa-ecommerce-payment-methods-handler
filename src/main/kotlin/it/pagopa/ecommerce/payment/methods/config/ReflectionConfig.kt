eaa46333e223f10301be59996a718911cf632a6a
Forciniti Luigi <forciniti@spi.srl>
2026-03-12 16:15:14 +0100

    chore: replace targets with classNames in RegisterForReflection

--- src/main/kotlin/it/pagopa/ecommerce/payment/methods/config/ReflectionConfig.kt
+++ src/main/kotlin/it/pagopa/ecommerce/payment/methods/config/ReflectionConfig.kt
@@ -1,20 +1,15 @@
 package it.pagopa.ecommerce.payment.methods.config
 
 import io.quarkus.runtime.annotations.RegisterForReflection
-import it.pagopa.ecommerce.payment.methods.v1.server.model.ProblemJson
-import it.pagopa.generated.ecommerce.client.model.PaymentNoticeItemDto
-import it.pagopa.generated.ecommerce.client.model.PaymentOptionMultiDto
-import it.pagopa.generated.ecommerce.client.model.PspSearchCriteriaDto
-import it.pagopa.generated.ecommerce.client.model.TransferListItemDto
 
 @RegisterForReflection(
-    targets =
+    classNames =
         [
-            ProblemJson::class,
-            PaymentNoticeItemDto::class,
-            TransferListItemDto::class,
-            PaymentOptionMultiDto::class,
-            PspSearchCriteriaDto::class,
+            "it.pagopa.ecommerce.payment.methods.v1.server.model.ProblemJson",
+            "it.pagopa.generated.ecommerce.client.model.PaymentNoticeItemDto",
+            "it.pagopa.generated.ecommerce.client.model.TransferListItemDto",
+            "it.pagopa.generated.ecommerce.client.model.PaymentOptionMultiDto",
+            "it.pagopa.generated.ecommerce.client.model.PspSearchCriteriaDto",
         ]
 )
 class ReflectionConfig {}
