7d02d8729d9158527f49491c4f6f14014144c8a5
Dylan Tangredi <dylan.tangredi.ext@jakala.com>
2026-05-21 17:17:47 +0200

    feat: [PIDM-1977] add npg sdk integrity endpoint with caching

--- src/main/kotlin/it/pagopa/ecommerce/payment/methods/config/ReflectionConfig.kt
+++ src/main/kotlin/it/pagopa/ecommerce/payment/methods/config/ReflectionConfig.kt
@@ -1,6 +1,8 @@
 package it.pagopa.ecommerce.payment.methods.config
 
 import io.quarkus.runtime.annotations.RegisterForReflection
+import it.pagopa.ecommerce.payment.methods.v1.server.model.NpgSdkIntegrityResponse
 import it.pagopa.ecommerce.payment.methods.v1.server.model.ProblemJson
 
-@RegisterForReflection(targets = [ProblemJson::class]) class ReflectionConfig {}
+@RegisterForReflection(targets = [ProblemJson::class, NpgSdkIntegrityResponse::class])
+class ReflectionConfig {}
