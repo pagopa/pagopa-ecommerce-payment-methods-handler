ffd9ccdeb18df58dd7a8f9bc5f54642c6071b539
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2026-06-04 12:01:38 +0200

    fix api key header param

--- src/main/kotlin/it/pagopa/ecommerce/payment/methods/client/NpgRestClient.kt
+++ src/main/kotlin/it/pagopa/ecommerce/payment/methods/client/NpgRestClient.kt
@@ -19,7 +19,7 @@ interface NpgRestClient {
     @Produces(MediaType.APPLICATION_JSON)
     fun buildForm(
         @HeaderParam("Correlation-Id") correlationId: String,
-        @HeaderParam("apikey") apiKey: String,
+        @HeaderParam("X-API-KEY") apiKey: String,
         request: NpgBuildRequest,
     ): Uni<NpgBuildResponse>
 }
