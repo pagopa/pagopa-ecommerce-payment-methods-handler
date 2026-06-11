707bf5776bd687300362a3693e3129c75c77c4b5
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2026-06-11 15:54:31 +0200

    fix log info

--- src/main/kotlin/it/pagopa/ecommerce/payment/methods/services/PaymentMethodServiceImpl.kt
+++ src/main/kotlin/it/pagopa/ecommerce/payment/methods/services/PaymentMethodServiceImpl.kt
@@ -166,7 +166,7 @@ constructor(
         xClientId: String,
     ): Uni<CreateSessionResponse> {
         log.info(
-            "[Payment Method service] create new NPG session using paymentMethodId: {}",
+            "[Payment Method handler] create new NPG session using paymentMethodId: {}",
             paymentMethodId,
         )
 
@@ -271,7 +271,7 @@ constructor(
         xClientId: String,
     ): Uni<SessionPaymentMethodResponse> {
         log.info(
-            "[Payment Method service] Retrieve card data from NPG using paymentMethodId: {} and orderId: {}",
+            "[Payment Method handler] Retrieve card data from NPG using paymentMethodId: {} and orderId: {}",
             paymentMethodId,
             orderId,
         )
